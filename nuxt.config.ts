a36bc62abbbd9359a8ab98ad5f4409cf3b3fcd6a
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-09-26 17:01:14 +0800

    setup: add Nuxt "noScripts: true" option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,6 +109,7 @@ export default defineNuxtConfig({
     ],
   },
   experimental: {
+    noScripts: true,
     // when using generate, payload js assets included in sw precache manifest
     // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
