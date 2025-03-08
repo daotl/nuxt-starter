a3fc7bc8923c66b1a6e311c3c0b6180a2f542ef8
Nex Zhu <hitnexup@gmail.com>
2025-03-08 18:47:33 +0800

    setup: add nitro.devProxy to proxy backend APIs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -65,6 +65,13 @@ export default defineNuxtConfig({
       routes: ['/'],
       ignore: ['/hi'],
     },
+    // Proxy backend APIs
+    // devProxy: {
+    //   '/api': {
+    //     target: 'http://local.dev:8080/api',
+    //     changeOrigin: true,
+    //   },
+    // },
   },
   typescript: {
     strict: true,
